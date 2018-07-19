6c7395ec9a75208fabb0ddb06e261bec9baae94c
l-wiewiora <wiewiora.luk@gmail.com>
2018-07-19 02:34:03 +0200

    Removed unnecessary files, updated layout, added simple scripts and gui behavior

---
+++ TernopilDotNet/Controllers/CommentsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TernopilDotNet.Models;
+
+namespace TernopilDotNet.Controllers
+{
+    [Route("api/[controller]")]
+    public class CommentsController : Controller
+    {
+        // TODO: replace with database
+        private static List<string> comments = new List<string>();
+        public CommentsController(IConfiguration config)
+        {
+        }
+
+        [HttpGet]
+        public List<string> GetComments()
+        {
+            return comments;
+        }
+
+        [HttpPost]
+        public void SaveComment([FromQuery]string comment)
+        {
+            comments.Add(comment);
+        }
+    }
+}
--- TernopilDotNet/wwwroot/js/site.js
+++ TernopilDotNet/wwwroot/js/site.js
@@ -1,1 +1,29 @@
-﻿// Write your JavaScript code.
+﻿$(document).ready(function() {
+    getComments();
+});
+
+$('#save-data-btn').click(function() {
+    $.ajax({
+        url: 'api/comments?comment=' + $('#comment').val(),
+        method: 'POST'
+    }).done(function() {
+        getComments();
+    });
+});
+
+function getComments() {
+    $.ajax({
+        url: 'api/comments',
+        method: 'GET'
+    }).done(function (data) {
+        $('#comments-section').empty();
+        $.each(data,
+            function (index, comment) {
+                if (comment != null) {
+                    var newItem = document.createElement('li');
+                    $(newItem).text(comment);
+                    $('#comments-section').append(newItem);
+                }
+            });
+    });
+}
